eefb66adef2fcb2f2a64270f8564e80c4f697a23
gudeaguiar <gudeaguiar@gmail.com>
2024-08-29 23:32:51 -0300

    Criacao de file de conta e primeiras adicoes a ela

---
+++ atena-porto-seguro/src/style/styled-conta.ts
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export const StyleContaContainer = styled.div`
+    margin-top: 6vh;
+    h2 {
+    text-align: center;
+    margin-bottom: 5vh;
+    }
+`;
+
+export const StyleConta = styled.div`
+    
+    display: grid;
+    justify-items: center;
+    grid-auto-flow: column;
+    gap: 4vh;
+    align-items: center;
+    text-align: center;
+    height: 52vh;
+    `;
