9939301ac2a3cbc94d779fc9d0f0af779b0f9081
gudeaguiar <gudeaguiar@gmail.com>
2024-08-29 23:59:25 -0300

    Criacao de file funcionalidades e adicao de estilizacoes nela e ajuste na conta

---
+++ atena-porto-seguro/src/style/styled-funcionalidades.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const StyleGradient = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    background: linear-gradient(to bottom, #68ee86, #3da3d0);
+    flex-wrap: wrap;
+    align-content: center;
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+    text-align: center;
+    position: fixed;
+    `;
