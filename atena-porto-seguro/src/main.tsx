a4a6db5a934916154064343cbf01dec90f09ef10
nathan <nathan.consolo17@gmail.com>
2024-08-30 22:44:04 -0300

    retirando componente que n foi utilizado

--- atena-porto-seguro/src/main.tsx
+++ atena-porto-seguro/src/main.tsx
@@ -1,4 +1,3 @@
-import { StrictMode, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom'
