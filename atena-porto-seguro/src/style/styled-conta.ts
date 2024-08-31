a3e7bfb8e933d0dbdd5ad2f71bd32e52ea75ea15
Nanfer12007 <fenix12007@gmail.com>
2024-08-30 23:08:35 -0300

    media querry conta adicionado

--- atena-porto-seguro/src/style/styled-conta.ts
+++ atena-porto-seguro/src/style/styled-conta.ts
@@ -43,23 +43,99 @@ export const StyleButtonConta = styled.button`
     margin-right: 24vw;       
 `;
 
-export const StyleCabecalhoVerde = styled.header`
 
-    display: flex;
-    height: 6rem;
-    flex-direction: row;
-    justify-content: start;
-    align-items: center;
-    position: relative;
-    background-color: #68ee86;
-    background-image: url(../imgs/logo-portoseguro.webp);
-    background-size: 22rem;
-    background-position: right;
-    background-repeat: no-repeat;
-    padding-left: 5vw;
-    color: #3da3d0;
-    font-size: 2vw;
+export const StyleMedia = styled.media`
+    @media screen and (max-width: 4400px) {
+        #conta {
+            grid-template-columns: 1fr 1fr 1fr;
+            grid-template-rows: 20vh 20vh;
+        }
+        #btn-conta div {
+            display: flex;
+            flex-direction: row;
+            justify-content: space-around;
+            align-items: center;
+            width: 14vw;
+            img {
+                width: 45px;
+                height: 45px;
+            }
+        }
+
+        button {
+            height: 60px;
+            width: 200px;
+        }
+
+        .cabecalho-verde {
+            display: flex;
+            height: 6rem;
+            flex-direction: row;
+            justify-content: start;
+            align-items: center;
+            position: relative;
+            background-color: #68ee86;
+            background-image: url(../imgs/logo-portoseguro.webp);
+            background-size: 22rem;
+            background-position: center;
+            background-repeat: no-repeat;
+            padding-left: 5vw;
+            color: #3da3d0;
+            font-size: 1vw;
+        }
+    }
 `;
+
+export const StyleMedia2 = styled.media`
+    @media screen and (max-width: 1440px) {
+        #conta {
+            grid-template-columns: 1fr 1fr;
+            grid-template-rows: 12vh 12vh 12vh;
+            font-size: 75%;
+
+            input {
+                height: 4vh;
+            }
+        }
+
+        button {
+            height: 40px;
+            width: 160px;
+            margin-left: 10vw;
+        }
+
+        #btn-conta div {
+            display: flex;
+            flex-direction: column;
+            font-size: 75%;
+            text-align: center;
+            align-items: center;
+
+            img {
+            width: 30px;
+            height: 30px;
+            }
+        }
+
+        .cabecalho-verde {
+            display: flex;
+            height: 6rem;
+            flex-direction: row;
+            justify-content: start;
+            align-items: center;
+            position: relative;
+            background-color: #68ee86;
+            background-image: url(../imgs/logo-portoseguro.webp);
+            background-size: 22rem;
+            background-position: right;
+            background-repeat: no-repeat;
+            padding-left: 5vw;
+            color: #3da3d0;
+            font-size: 2vw;
+        }
+    }
+`;
+
 
 
 
