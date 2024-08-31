8d266713741c45f51fafdfa324af06894a391123
Nanfer12007 <fenix12007@gmail.com>
2024-08-30 23:23:43 -0300

    Criacao Componente Cabecalho Verde

--- atena-porto-seguro/src/components/CabecalhoVerde/CabecalhoVerde.tsx
+++ atena-porto-seguro/src/components/CabecalhoVerde/CabecalhoVerde.tsx
@@ -1,1 +1,11 @@
+import { StyleCabecalhoVerde } from "../../style/style-tela-inicial";
 
+export default function CabecalhoVerde() {
+    return (
+        <>
+        <StyleCabecalhoVerde>
+            <header></header>
+        </StyleCabecalhoVerde>
+        </>
+    )
+}
