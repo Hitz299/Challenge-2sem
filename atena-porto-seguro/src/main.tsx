8c43b25a0cfcb80f61eb75a97390e88dfcb4fba7
nathan <nathan.consolo17@gmail.com>
2024-09-02 10:48:12 -0300

    Criando lista de veiculos

--- atena-porto-seguro/src/main.tsx
+++ atena-porto-seguro/src/main.tsx
@@ -1,46 +1,48 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
 import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom'
-import Error from './routes/Error/index.tsx'
-import Login from './routes/Login/index.tsx'
-import EsqueciSenha from './routes/EsqueciSenha/index.tsx'
+import App from './App.tsx'
 import AdicionarVeiculos from './routes/AdicionarVeiculos/index.tsx'
-import CriarConta from './routes/CriarConta/index.tsx'
-import Home from './routes/Home/index.tsx'
-import LocalizarVeiculos from './routes/LocalizarVeiculos/index.tsx'
-import RedefinirSenha from './routes/RedefinirSenha/index.tsx'
-import MinhaConta from './routes/MinhaConta/index.tsx'
 import ChatBot from './routes/ChatBot/index.tsx'
-import MeusVeiculos from './routes/MeusVeiculos/index.tsx'
+import CriarConta from './routes/CriarConta/index.tsx'
+import Error from './routes/Error/index.tsx'
+import EsqueciSenha from './routes/EsqueciSenha/index.tsx'
+import Home from './routes/Home/index.tsx'
 import Integrantes from './routes/Integrantes/index.tsx'
+import LocalizarVeiculos from './routes/LocalizarVeiculos/index.tsx'
+import Login from './routes/Login/index.tsx'
+import MeusVeiculos from './routes/MeusVeiculos/index.tsx'
+import MinhaConta from './routes/MinhaConta/index.tsx'
+import RedefinirSenha from './routes/RedefinirSenha/index.tsx'
 
 const routes = createBrowserRouter([
-  {path:"/", element:<App/>,errorElement:<Error/>,children:
-  [
-     {
-        path: "/",
-        element: <Navigate to="/login" replace />
-      },
-    {path:"/login",element:<Login/>},
-    {path:"/login/recuperar/senha",element:<EsqueciSenha/>},
-    {path:"/login/recuperar/senha/redefinir/senha",element:<RedefinirSenha/>},
-    {path:"/login/criar/conta",element:<CriarConta/>},
-    {path:"/home",element:<Home/>},
-    {path:"/home/adicionar/veiculos",element:<AdicionarVeiculos/>},
-    {path:"/home/integrantes",element:<Integrantes/>},
-    {path:"/home/meus/veiculos",element:<MeusVeiculos/>},
-    {path:"/home/chatbot",element:<ChatBot/>},
-    {path:"/home/minha/conta",element:<MinhaConta/>},
-    {path:"/home/meus/veiculos/localizar/veiculos",element:<LocalizarVeiculos/>},
-  ]}
-  ])
+  {
+    path: "/", element: <App />, errorElement: <Error />, children:
+      [
+        {
+          path: "/",
+          element: <Navigate to="/login" replace />
+        },
+        { path: "/login", element: <Login /> },
+        { path: "/login/recuperar/senha", element: <EsqueciSenha /> },
+        { path: "/login/recuperar/senha/redefinir/senha", element: <RedefinirSenha /> },
+        { path: "/login/criar/conta", element: <CriarConta /> },
+        { path: "/", element: <Home /> },
+        { path: "/home/adicionar/veiculos", element: <AdicionarVeiculos /> },
+        { path: "/home/integrantes", element: <Integrantes /> },
+        { path: "/home/meus/veiculos", element: <MeusVeiculos /> },
+        { path: "/home/chatbot", element: <ChatBot /> },
+        { path: "/home/minha/conta", element: <MinhaConta /> },
+        { path: "/home/meus/veiculos/localizar/veiculos", element: <LocalizarVeiculos /> },
+      ]
+  }
+])
 
 
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={routes}/>
+    <RouterProvider router={routes} />
   </StrictMode>
 )
---
+++ atena-porto-seguro/src/script/listaVeiculos.ts
@@ -0,0 +1,22 @@
+export const listaVeiculos = [
+    {
+        modelo: "Fusca",
+        placa: "ABC-1234",
+        cor: "azul",
+    },
+    {
+        modelo: "Civic",
+        placa: "XYZ-5678",
+        cor: "preto",
+    },
+    {
+        modelo: "Hilux",
+        placa: "LMN-9012",
+        cor: "prata",
+    },
+    {
+        modelo: "Onix",
+        placa: "GHI-3456",
+        cor: "vermelho",
+    }
+];
