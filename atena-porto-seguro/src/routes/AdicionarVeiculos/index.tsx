010f843ca5a5c639d14e3e5e53d83566e8983761
nathan <nathan.consolo17@gmail.com>
2024-09-10 16:39:29 -0300

    Metodo de teste para cadastro dos veiculos

--- atena-porto-seguro/src/routes/AdicionarVeiculos/index.tsx
+++ atena-porto-seguro/src/routes/AdicionarVeiculos/index.tsx
@@ -1,15 +1,39 @@
 
+import React, {useState} from "react";
+import BgGradient from "../../components/BgGradient/BgGradient";
+import Button from "../../components/Button/Button";
 import CabecalhoVerde from "../../components/CabecalhoVerde/CabecalhoVerde";
 
 
 export default function AdicionarVeiculos() {
 
     document.title = "Cadastrar Veículos";
-   
+
+    const [icones, setIcones] = useState<JSX.Element[]>([]);
+
+  const adicionarIcone = () => {
+    let index: number  = 0;
+    setIcones([...icones, <Button titulo="aaa" click={() => {}} key={index} />]);
+    index ++;
+  };
+
   return (
     <>
       <CabecalhoVerde titulo="Adiconar Veículos"/>
 
+
+      <BgGradient>
+
+
+          <Button titulo="Criar" click={adicionarIcone}/>
+
+           <div>
+          {icones.map((icone, index) => (
+            <div key={index}>{icone}</div>
+          ))}
+        </div>
+
+      </BgGradient>
     </>
   )
 }
